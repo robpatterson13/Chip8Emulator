0250c6137e757f04c794a818cbd569f8901a0ce6
robpatterson13 <robertpatterson1313@gmail.com>
2024-04-10 17:55:20 -0400

    Initial Commit

---
+++ Chip8Emulator/Chip8EmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chip8EmulatorApp.swift
+//  Chip8Emulator
+//
+//  Created by Rob Patterson on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Chip8EmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chip8Emulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chip8Emulator
+//
+//  Created by Rob Patterson on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
