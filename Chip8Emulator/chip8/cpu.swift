bae60f2f365e4ee44416fe0bf28689c29455cdb0
robpatterson13 <robertpatterson1313@gmail.com>
2024-04-10 18:18:38 -0400

    initialized CPU file

--- Chip8Emulator/chip8/cpu.swift
+++ Chip8Emulator/chip8/cpu.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class Chip8 {
+    var memory: [UInt8]
+    
+    var display: [[CFBit]]
+    
+    var PC: UInt16
+    var I: UInt16
+    
+    var stack: [UInt16]
+    
+    var delayTimer: UInt8
+    var soundTimer: UInt8
+    
+    var V: [UInt8]
+    
+    init() {
+        self.memory = []
+        self.display = [[]]
+        self.PC = 0
+        self.I = 0
+        self.stack = []
+        self.delayTimer = 0
+        self.soundTimer = 0
+        self.V = []
+    }
+}
